4b13bd0dc2c9eeec6155658d1072b1c1c2432b09
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-10-23 19:44:02 +0700

    try

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -20,9 +20,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
                     return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi', quote=True)
 
         if key == hastag[0]:
-            picture = config.pic_girl
         elif key == hastag[1]:
-            picture = config.pic_boy
         
         link = await get_link()
         caption = msg.text or msg.caption
