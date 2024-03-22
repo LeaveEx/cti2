c2082826ec08f98340f1b33719d8ed86167b8e02
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2024-03-23 03:03:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 
 api_id = int(os.environ.get("API_ID", "2857053"))
 api_hash = os.environ.get("API_HASH", "4a01e3596661ba4bf609d15c1f9e129b")
-bot_token = os.environ.get("BOT_TOKEN", "66057365695:AAGLG5qu7Xa3QgrDOO91ARkZzFRbxaH6gEE")
+bot_token = os.environ.get("BOT_TOKEN", "6057365695:AAGLG5qu7Xa3QgrDOO91ARkZzFRbxaH6gEE")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://muhamadfaiz:muhamadfaiz@cluster0.mpwspud.mongodb.net/?retryWrites=true&w=majority")
