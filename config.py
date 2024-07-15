e3362b174b3988912adf88befe2aae8e286637a0
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2024-07-15 08:22:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 import logging
 
-api_id = int(os.environ.get("API_ID", "2857053"))
-api_hash = os.environ.get("API_HASH", "4a01e3596661ba4bf609d15c1f9e129b")
-bot_token = os.environ.get("BOT_TOKEN", "6057365695:AAGLG5qu7Xa3QgrDOO91ARkZzFRbxaH6gEE")
+api_id = int(os.environ.get("API_ID", "23138248"))
+api_hash = os.environ.get("API_HASH", "fb765fe307cf28e295da71ac2c3d17f2")
+bot_token = os.environ.get("BOT_TOKEN", "7421669727:AAFPbb3vKVU9elO0EcqMC5ecL_V-aE1k7hU")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://muhamadfaiz:muhamadfaiz@cluster0.mpwspud.mongodb.net/?retryWrites=true&w=majority")
 db_name = os.environ.get("DB_NAME", "telegram")
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "-1001439465967"))
-channel_2 = int(os.environ.get("CHANNEL_2", "-1001215547362"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1002003868172"))
+channel_1 = int(os.environ.get("CHANNEL_1", "-1001953908892"))
+channel_2 = int(os.environ.get("CHANNEL_2", "-1001959200512"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1002229550163"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "2100705176"))
+id_admin = int(os.environ.get("ID_ADMIN", "6350981043"))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "2"))
@@ -24,7 +24,7 @@ batas_kirim = int(os.environ.get("BATAS_KIRIM", "2"))
 biaya_kirim = int(os.environ.get("BIAYA_KIRIM", "25"))
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#kowkwk #nyetsa #ctngirl #ctnboy #ctnask #ctnrandom #ctnmutualan #ctncurhat").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#kowkwk #nyetsa #atgirl #atboy #atask #atrandom #atmutualan #atcurhat").replace(" ", "|").lower()
 # =========================================================== #
 
 pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph/file/b2f62b0642bf0fd5849a8.jpg")
@@ -32,23 +32,23 @@ pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph/file/c9d103073f89b0568
 # =========================================================== #
 
 pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
-start_msg = os.environ.get("START_MSG", """Halo {mention} ⭐\n\n<b>CHANNEL CTN</b> adalah Bot Auto Post, Semua Pesan Yang Kamu Kirim Akan Masuk Ke Channel @SIARANCTN. Silahkan kirim pesan anda menggunakan hastag dibawah ini:
+start_msg = os.environ.get("START_MSG", """Halo {mention} 🔥\n\n<b>MENFESS RESFAMS</b> adalah Bot Auto Post, Semua Pesan Yang Kamu Kirim Akan Masuk Ke Channel @menfes_resfams. Silahkan kirim pesan anda menggunakan hastag dibawah ini:
 
-#ctnboy (Untuk Identitas Laki-Laki)
-#ctngirl (Untuk Identitas perempuan)
-#ctnrandom (Untuk hal random)
-#ctnmutualan (Untuk mutualan)
-#ctnask (Untuk membagikan hal bodoh)
-#ctncurhat (Untuk curhat masalah)
+#atboy (Untuk Identitas Laki-Laki)
+#atgirl (Untuk Identitas perempuan)
+#atrandom (Untuk hal random)
+#atmutualan (Untuk mutualan)
+#atask (Untuk membagikan hal bodoh)
+#atcurhat (Untuk curhat masalah)
 """)
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
 {mention}, Pesan Mu Gagal Terkirim Silahkan Gunakan Hashtag Berikut:
 
-#ctnboy (Untuk Identitas Laki-Laki)
-#ctngirl (Untuk Identitas perempuan)
-#ctnrandom (Untuk hal random)
-#ctnmutualan (Untuk mutualan)
-#ctnask (Untuk membagikan hal bodoh)
-#ctncurhat (Untuk curhat masalah)
+#atboy (Untuk Identitas Laki-Laki)
+#atgirl (Untuk Identitas perempuan)
+#atrandom (Untuk hal random)
+#atmutualan (Untuk mutualan)
+#atask (Untuk membagikan hal bodoh)
+#atcurhat (Untuk curhat masalah)
 """)
